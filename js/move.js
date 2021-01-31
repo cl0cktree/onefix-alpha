bb61d04165e375a5b238e5a1416ffaad3ebabd7d
Lee.Hyunsup <clocktree@live.co.kr>
2021-02-01 00:40:00 +0900

    20210201_2
    - 수정.

--- js/move.js
+++ js/move.js
@@ -4,6 +4,11 @@ $(function(){
 			$('.rading-page').css({'display':'none'})
 		});
 	});
-	
+	$('.video_btn').on('click',function(){
+		$('.background_filter').fadeIn('300');
+	});
+	$('.video_close_btn').on('click',function(){
+		$('.background_filter').fadeOut('300');
+	});
 	return false;
 });
